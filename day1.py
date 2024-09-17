0211b7dc283fa4bf54dd7ef4600457d668b4b07a
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-09-17 18:15:27 +0530

    day1 code

---
+++ day1.py
@@ -0,0 +1,60 @@
+# _ = 10
+# print(_) 
+# print(type(_))
+
+# var1 = -10 
+# var2 = 10.25 
+# var3 = True 
+# var4 = 'upflairs' 
+# print(var1)
+# print(type(var1))
+
+# print(var2)
+# print(type(var2))
+
+# print(var3)
+# print(type(var3))
+
+# print(var4)
+# print(type(var4))
+
+
+# var1 = 'upflairs' 
+# var2 = "upflairs"
+# var3  = """upflairs"""
+# var4 ='''upflairs'''
+
+# print(var1)
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# print(type(var4))
+
+# string 
+# <class 'str'>
+# <class 'str'>
+# <class 'str'>
+# <class 'str'>
+
+
+
+# single qu
+# ates vsd double quates 
+# city = "mummai"
+# profession = "softwere engineer"
+# company_name = "upflairs"
+
+
+# myself = f'''hey this is radhey from {city},
+#         i am working as {profession} at {company_name},'''
+
+
+# myself = '''hey this is radhey from {},
+#         i am working as {} at {},'''.format(city,profession,company_name)
+
+# print(myself)
+
+company_name = "upflairs" 
+print(company_name[-7::-1])
+
+
