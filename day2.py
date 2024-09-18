aa769b1781f01d582d0d26969f7a001eb53bcd47
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-09-18 18:18:24 +0530

    string done

---
+++ day2.py
@@ -0,0 +1,57 @@
+# st = "upflairs" 
+# print(len(st))   # 8  character 
+# # built in function  
+# print(st.upper())
+
+# st = 'upflairs pvt ltd jaipur rajasthan'
+# print(st.lower())
+# print(st.title())   # it convert first letter of your word into uppercase 
+# print(st.capitalize())  # first letter  
+# print(st.swapcase())  #lower ==> upper, upper ==> lower 
+# print(st.count('jaipurs'))
+# print(st.endswith('anj'))
+# print(st.startswith('upf'))
+
+
+# print(st.center(20,"*"))
+
+# print(st.ljust(8+6,'*') )
+# st.rjust()
+# st.rjust()
+# print(st.index('p',10))
+# st = " "
+# print(st.find("z")) 
+# print(st.isalpha()) 
+# print(st.isalnum())  
+# print(st.isdigit()) 
+# print(st.isspace())   # only space return True otherwise return False 
+# st = "                  upflairs                  "
+# print(st)
+# st2  = st.strip() 
+# print(st2)
+# print(st)
+
+# print(len(st)) 
+# print(st)
+# print(st)
+# print(st.strip())
+# print(st)
+# print(st.lstrip())
+# print(st.rstrip())
+
+st = "upflairs"
+st.isdecimal()
+st.isnumeric()
+st.istitle()
+st.isupper()
+st.islower()
+st.expandtabs() 
+st.removeprefix() 
+st.removesuffix() 
+st.rfind()
+st.rindex() 
+st.replace()
+st.split()
+st.rsplit()
+st.splitlines()
+st.translate()
