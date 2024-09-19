c1fff4ae9529245cb49ca19d59d50aa25d8a81c5
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-09-19 18:19:12 +0530

    list code

---
+++ day3.py
@@ -0,0 +1,48 @@
+# ls = [10,20,30,50,60,70,80,90,25.2,36.5,'upflairs',True]
+# print(ls) 
+# print(type(ls))
+
+# ls = [25,45,63,96,85,74,52]
+# ls[2] = 100
+# print(ls) 
+# ls.append(500)   # to insert the item at the last position 
+# ls.pop()  # to remove the last item by default 
+# ls.pop(0)   # to remove the item from the index position using pop 
+
+
+# ls = [25,45,63,96,85,74,52,63]
+# ls.insert(0,1000)  # it is used to insert item at a specific position 
+# ls.remove(63)   # remove 
+# del ls[2]
+# ls.clear()  # empty 
+# print(ls.count(630) )
+# ls.reverse()  # ls[::-1]
+# print(ls)
+# print(ls.index(45)) 
+# ls.sort()   # ascending order
+# sorted(ls)   # ascending order 
+# print(ls[::-1])  # descending order 
+# ls.sort(reverse=True)  # first sort in ascending order then reverse the list
+
+ls1 = [25,45,63,96,85,74,52,63]
+ls2 = ls1 
+# del ls2 
+ls2.append(5000)
+print(ls1) 
+ls1.extend()
+
+# ls3 = ls1.copy()   
+# ls3.pop(1)
+# print("list 2 : ",ls3)
+# print("list 1 : ",ls1)
+
+# print(ls2)
+# print(ls3)
+# num1 = 100 
+# num2 = num1 
+# num2 = 200 
+# del num2
+# print(num1)
+
+
+
