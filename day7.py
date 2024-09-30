c99847edde0ff15cdb530846c072fcc00fac7b44
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-09-30 18:29:21 +0530

    day7

---
+++ day7.py
@@ -0,0 +1,63 @@
+### operators 
+# 1. Arithmatic 
+# 2. Assignment 
+# 3. comparison 
+# 4. Logical operators (AND , OR , NOT )
+## 
+# AND   (both condition true )
+# A    B   OUTPUT 
+# 0    0     0 
+# 0    1     0 
+# 1    0     0
+# 1    1     1
+
+# OR   (if single condition true return true  )
+# A    B   OUTPUT 
+# 0    0     0 
+# 0    1     1 
+# 1    0     1
+# 1    1     1 
+
+# NOT  (VICE VERSA)
+# True  ==> False 
+# False ==> True 
+# print(not(True))
+
+
+#  conditional statement  (if-else-block )
+
+# num = 52 
+# if num >= 52:
+#     print('number is greater than')
+    
+# else:
+#     print("Number is less than 52")
+
+# student_name = "RAMESH"
+# student_name_ls  = ['RAHUL','MOHIT','ROCKY','MADHU','JYOTI','KAREENA']
+# neighbours_name_ls = ['RAHUL','SURESH','ROCKY','RAJU','PREEETI','KAALU']
+
+# if student_name not in student_name_ls : 
+#     print("i am not your classmate")
+# else:
+#     print('he is you classmate')
+
+# if  student_name in student_name_ls:   # True 
+#     print('yES IT IS YOUR CLASSMATE') 
+
+# elif student_name in neighbours_name_ls:
+#     print("he is my neighbours")
+# else:
+#     print('this is not my classmate') 
+
+# Num1 = 20 
+# num2 = 20.0
+# print(Num1 is not num2)
+# print(Num1 == num2)
+
+
+
+
+
+
+
