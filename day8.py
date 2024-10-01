a64c05d5ee4258c409484d0bd958df0c5c944f48
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-10-01 18:30:13 +0530

    for loop

---
+++ day8.py
@@ -0,0 +1,71 @@
+# for i in range(6,20,2): 
+#     print(i)
+
+# for i in range(9,-1,-1):
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+## array traversing 
+# ls = [10,20,30,40,50,60,70,80,90]
+# print(len(ls))
+# for i in range(len(ls)):
+#     print(ls[i])
+
+# for item in ls:
+#     print(item)
+
+# st = "upflairs"
+# for char in st: 
+#     print(char,end='  ')
+
+
+# tpl = (10,20,30,40,50,60,70,80,90)
+# for item in tpl:
+#     print(item)
+
+
+# st = {10,20,30,40,50,60,70,80,90}
+# for item in st:
+#     print(item)
+
+# print(dict.fromkeys(list(range(5,25)),"unknown"))
+
+# ls = ['A','B','C','D','E','F']
+# dt = dict.fromkeys(ls,'unknown')
+# print(dt)
+
+dt = {'A':10,'B':20,'C':30,'D':40}
+# print(dt.items())
+# for item in dt.keys():
+#     print(item)
+
+# for item in dt.values():
+#     print(item)
+
+# for item in dt.items():
+#     # print(item)
+#     print("key : ",item[0],"  >>>>>   ",item[1]) 
+
+# for key , vlau in dt.items():
+#     print(key )
+#     print(valu)
+
+
+# enumerate
+# tpl = (10,20,30,40,50,60,70,80,90)
+# count = 0  
+# for item in tpl:
+#     print(item,"  ===>  ",count)
+#     count +=1 
+
+
+tpl = (10,20,30,40,50,60,70,80,90)
+  
+for i, item in enumerate(tpl):
+    print(item,"  ===>  ",i," <==>  ",item,"  ===>  ",-((len(tpl)+1)-i-1))
+
+
+
+ 
