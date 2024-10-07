76b57cfb39d5b36bba51e54f39efd230d642779e
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-10-07 18:22:45 +0530

    day12

---
+++ day12.py
@@ -0,0 +1,67 @@
+# def test(num1,num2):
+#     # return  "Hii i am exected" 
+#     # print("hiiii i m executing")
+#     result = num1 + num2 
+#     return result
+
+# n1 = int(input('Enter your first number : '))
+# n2 = int(input('Enter your second number : '))
+# output = test(num1=n1,num2=n2) 
+# print(output)
+
+# def total_sum(ls):
+#     totalsum = 0
+#     for item in ls:
+#         totalsum += item
+#     return totalsum
+
+# inputdata_ls = [10,20,30,40,50,60,70,80,90]
+# output = total_sum(ls=inputdata_ls)
+# print("Your sum is : ",output)
+
+# def item_filter(ls):
+#     filter_data = [ ]
+#     for item in ls: 
+#         if item <= 50 :
+#             filter_data.append(item)
+#     return filter_data
+
+# inputdata_ls  = [52,36,75,95,66,55,44,22,15,36,87,78,94,42,36] 
+# output = item_filter(ls=inputdata_ls)
+# print("your item are here : ",output)
+
+
+# def test(num1=20,num2=52):
+#     return num1 + num2 
+
+# print(test(num1=30))
+
+# def total_sum(*data):
+#     print(type(data))
+#     for item in data:
+#         print(item)
+
+
+# total_sum(10,20,30,40,50,60,70,80,90,50,45,60,75,84,86,33,55,96,78,23,36)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
