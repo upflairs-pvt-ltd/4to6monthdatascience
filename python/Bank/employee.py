262acd7f1b98308b1c6aaf1c0cf030ae5ba0a7ba
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-10-21 18:19:55 +0530

    modules

---
+++ python/Bank/employee.py
@@ -0,0 +1,73 @@
+# import manager
+# # manager.show_anshika_details() 
+# print(manager.employe_name)
+# print(manager.employee_id)
+# print(manager.employee_pass)
+
+
+
+# import manager as m 
+# # manager.show_anshika_details() 
+# print(m.employe_name)
+# print(m.employee_id)
+# print(m.employee_pass)
+
+
+
+# import os 
+# import sys 
+# import time 
+# import dataclasses 
+# import mysql 
+# import math 
+# import managerr 
+# print("modules are imported")
+
+
+# import manager 
+# print(manager.employee_id) 
+# print(manager.employee_pass)
+
+
+
+# to access specific block of code from the module 
+# from manager import employee_id
+# from manager import employee_pass
+# # OR  
+# # from manager import employee_id,employee_pass
+# print(employee_id)
+# print(employee_pass)
+
+
+
+
+# to access specific block of code from the module 
+# add alias 
+# from manager import employee_id as id 
+# from manager import employee_pass as password 
+# # OR  
+# # from manager import employee_id,employee_pass
+# print(id)
+# print(password)
+
+
+
+
+# from manager import show_anshika_details as emp_details 
+
+# emp_details()
+
+
+# from manager import *   # or import manager  
+# show_anshika_details()
+
+
+# from manager import * 
+# print(manager_id)
+# print(manager_password)
+
+
+
+from manager import show_anshika_details 
+# print("Employee file executed!")
+# print(__name__)
---
+++ python/Bank/manager.py
@@ -0,0 +1,31 @@
+
+
+n_employee = 10 
+employe_name = "anshika"
+employee_id = "anshika@gmail.com"
+employee_pass = "anshika823645"
+employee_salary = "400000"
+
+def show_manager_creditional():
+    print(manager_name)
+    print(manager_id)
+    print(manager_password)
+
+print("execute before condition")
+
+print("execute after condition")
+def show_anshika_details():
+    print(employe_name)
+    print(employee_id)
+    print(employee_pass)
+    print(employee_salary) 
+
+if __name__== "__main__":
+    manager_name = "dherendra shikhawat"
+    manager_id = "dheerendra@gmail.com"
+    manager_password = "ghjk852332"
+
+
+# print("manager file executed!")
+# print(__name__)
+
