b2fcebf7fcd39922392b45fa9e72d4c5a84e8549
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-11-05 18:26:32 +0530

    os module

---
+++ python/Bank/branch/copiedfile.py
@@ -0,0 +1,53 @@
+# from branch.branch_details import main_branch,no_of_employee 
+# from substaff.electrician_staff import electrician_incharge,electrician_address,electrician_salary 
+
+
+# print(main_branch) 
+# print(no_of_employee) 
+# print() 
+# print(electrician_incharge) 
+# print(electrician_salary)
+# print(electrician_address)
+
+
+# from substaff.electrician_staff import electrician_incharge
+# from  substaff.electrician_staff import electrician_salary 
+
+# from substaff.electrician_staff import *  
+# from substaff.electrician_staff import electrician_incharge as el_inchrg
+# print(el_inchrg)
+
+
+# module built-in , user-defined  
+# built-in:->  
+# csv, os , random , math , re 
+# python: web development , gui development , data science and machine learning 
+
+
+# data science and machine learning  with python: 
+# module
+# numpy ==> library or  package 
+# import numpy 
+# pip <== package manager  
+# import numpy
+# pandas install uninstall show 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
