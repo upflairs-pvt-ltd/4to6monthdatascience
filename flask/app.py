3a00787fe5b42ddc12a402f48fc8c253a002056c
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-12-09 14:18:05 +0530

    flask routes

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,7 @@
 
 ## templates  ==> webpages (html files)
 
-from flask import Flask,render_template
+from flask import Flask,render_template,render_template_string
 
 app = Flask(__name__)   # __main__ 
 
@@ -15,7 +15,33 @@ def Home():
 
 @app.route('/about')  # http://127.0.0.1:5000/about
 def about():
+    job_designation = "Data Scientist"
+    print(job_designation)
     return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+    print(job_designation)
+    return render_template('contact.html') 
+
+
+@app.route('/service')
+def service():
+    message = """
+    <html> 
+    <head>
+    <title> Testing </title>
+    <body>
+    <h1> Hey this SERvice page</h1>
+    </body>
+    </html>
+    """
+    return render_template_string(message)
+    # return message
+    # return "Hello this is service page!"
+    # return render_template('contact.html') 
+
 
 if __name__ == "__main__":
     app.run(debug=True)  
