4d674e337a192bb362cbafc999cd9fbf53d5e1f5
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-12-10 15:04:40 +0530

    flask

--- flask/app.py
+++ flask/app.py
@@ -3,10 +3,13 @@
 
 ## templates  ==> webpages (html files)
 
-from flask import Flask,render_template,render_template_string
+from flask import Flask,render_template,render_template_string,session,url_for,request
 
 app = Flask(__name__)   # __main__ 
+app.secret_key = "dkfd4343"
 
+# session = dict()  # {}   
+# session['name'] = 'radhey'
 @app.route('/')  # Home route 
 def Home():
     # return "Hey Great job, This is HOME PAGE"
@@ -15,14 +18,13 @@ def Home():
 
 @app.route('/about')  # http://127.0.0.1:5000/about
 def about():
-    job_designation = "Data Scientist"
-    print(job_designation)
+    session["job_designation"] = "Data Scientist"
     return render_template('about.html')
 
 
 @app.route("/contact")
 def contact():
-    print(job_designation)
+    print(session['job_designation'])
     return render_template('contact.html') 
 
 
@@ -41,6 +43,22 @@ def service():
     # return message
     # return "Hello this is service page!"
     # return render_template('contact.html') 
+
+
+@app.route("/userdata",methods=['GET','POST'])
+def userdata():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        contact = request.form['contact']
+        subject = request.form['subject']
+        message = request.form['message']
+        user_data = {"Name":name,"email":email,"contact":contact,"subject":subject,"message":message}
+        return  user_data 
+
+
+
+
 
 
 if __name__ == "__main__":
